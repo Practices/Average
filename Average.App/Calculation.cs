72fa6ca5f28856e3faaaaaa09d02917b34e06b44
iGilga <pyua87@gmail.com>
2015-11-27 16:17:49 +0300

    Добавлена функция для вычисления среднего аримфетического

--- Average.App/Calculation.cs
+++ Average.App/Calculation.cs
@@ -1,7 +1,18 @@
-﻿namespace Average.App
+﻿using System.Linq;
+
+namespace Average.App
 {
     public class Calculation
     {
-         
+        public double GetAverageArithmetric(int[] numbers)
+        {
+            int sum = numbers.Sum();
+
+            int count = numbers.Length;
+
+            double result = sum / count;
+
+            return result;
+        }
     }
 }
--- Average.UnitTest/CalculationAverageTest.cs
+++ Average.UnitTest/CalculationAverageTest.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Average.App;
+using Xunit;
 
 namespace Average.UnitTest
 {
     public class CalculationAverageTest
     {
+        private readonly Calculation calculation;
+
+        public CalculationAverageTest()
+        {
+            calculation = new Calculation();
+        }
+
+        [Fact]
+        public void ShouldBeReturnAverageArithmetric()
+        {
+            //arrange
+            var numbers = new int[] {1, 2, 3, 4, 5};
+
+            //act
+            var result = calculation.GetAverageArithmetric(numbers);
+
+            //assert
+            Assert.Equal(result, 3);          
+        }
     }
 }
